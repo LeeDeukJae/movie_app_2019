a869d2a3abd0bd84f05e0a4a388bf94dc25930d7
Hanpa <twilight5703@gmail.com>
2019-12-17 17:31:07 +0900

    Updates

---
+++ precache-manifest.56ae33731ca2b20a480728060edcbb18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d571274cbb791e07996fa2b3ac0f582",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "7602496c49bcbfd37d14",
+    "url": "/movie_app_2019/static/css/main.9b15ad1f.chunk.css"
+  },
+  {
+    "revision": "7ceb3f025d671189f167",
+    "url": "/movie_app_2019/static/js/2.db40e205.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2019/static/js/2.db40e205.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7602496c49bcbfd37d14",
+    "url": "/movie_app_2019/static/js/main.351fedfc.chunk.js"
+  },
+  {
+    "revision": "101d7acd549509bcf93b",
+    "url": "/movie_app_2019/static/js/runtime-main.69d003e7.js"
+  }
+]);
